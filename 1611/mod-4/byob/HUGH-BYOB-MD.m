4ba9000a79373b87a7e21c057a1ab7277d41ab95
Hugh Morrison <hmorri32@gmail.com>
2017-05-19 11:39:54 -0600

    CREATE HUGH-BYOB-MD.md
    byobackend README SUBMISSION SPEC

---
+++ 1611/mod-4/byob/HUGH-BYOB-MD.m
@@ -0,0 +1,75 @@
+# BYOB Submission Form
+
+[Project Spec](http://frontend.turing.io/projects/build-your-own-backend.html)
+
+------
+
+# Basics
+
+#### Link to the Github Repository for the Project
+[Yung REPO LINK](https://github.com/hmorri32/byo-backend)
+
+#### Link to the Deployed Application
+[Secret Depths - HEROKU](https://secure-depths-86331.herokuapp.com/)
+
+#### Link to Your Commits in the Github Repository for the Project
+
+-[Commits](https://github.com/hmorri32/byo-backend/commits/master)
+
+## Completion
+
+#### Were you able to complete the base functionality?
+
+* Seeded a database with at least 2 tables and 1 relationship?
+YES
+* Had at least 10 endpoints that returned responses with appropriate status codes?
+YES
+* Secured at least 4 endpoints with JWTs?
+YES
+* Enforced a linter and wrote code that conformed to it?
+YES
+* Wrote tests for both happy and sad paths for each endpoint?
+YES
+* Setup automatic deployments with CircleCI to a production app on Heroku?
+YES
+# Code Quality
+- Super great!
+
+#### Link to a specific block of your code on Github that you are proud of
+[Link to code block in repo](https://github.com/hmorri32/byo-backend/blob/master/routes/index.js#L122-L150)
+
+* Why were you proud of this piece of code?
+This was my first succesful PUT request! It solidified my knowledge of HTTP request as well as restful API basics. 
+It also cemented my knowledge surrounding how the database actually interacts with knex/express. Super cool stuff.
+I also implemented some ultra robust error handling. Which was neat. 
+
+#### Link to a specific block of your code on Github that you feel not great about
+[Link to code block in repo](https://github.com/hmorri32/byo-backend/blob/master/routes/index.js#L184-L213)
+
+* Why do you feel not awesome about the code? What challenges did you face trying to write/refactor it?
+This is a link to one of my handy PATCH works. I forced the request to include certain params and those params only. 
+I would have like for it to be more dynamic. IE, I wish I could have patched up one or many key value pairs as opposed to
+only the three I wrote. I would have gotten there, just ran out of time. 
+
+## Testing
+
+Attach a screenshot or paste the output from your terminal of the result of your test-suite running
+![yungtests](http://g.recordit.co/ASfqQl3Yuw.gif)
+
+## Linting
+Attach a screenshot or paste the output from your terminal of the result of your linter running
+-No errors. 
+![no errors](http://g.recordit.co/Vd2OTFsOqm.gif)
+
+-----
+
+#### Please feel free to ask any other questions or make any other statements below!
+
+Anything else you wanna say!
+
+This project was sweet as heck! Seriously. I enjoyed it tremendously. 
+-----
+
+# Instructor Feedback
+
+- Points: x / 150
